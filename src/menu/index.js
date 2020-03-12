8684854fac87e5ec0197a9e420e10c2b43fb4ee4
yutiansut <yutiansut@qq.com>
2020-03-12 15:55:38 +0800

    #

--- src/menu/index.js
+++ src/menu/index.js
@@ -43,33 +43,35 @@ export const menuHeader = [
     title: '首页',
     icon: 'home'
   },
-  // {
-  //   title: '功能',
-  //   icon: 'puzzle-piece',
-  //   children: [
-  //     demoD2Crud,
-  //     demoComponents,
-  //     demoElement,
-  //     demoCharts,
-  //     demoPlugins,
-  //     demoFrame,
-  //     {
-  //       title: '新窗口打开链接',
-  //       icon: 'link',
-  //       children: [
-  //         { path: 'https://github.com/d2-projects/d2-admin', title: 'D2Admin Github', icon: 'github' },
-  //         { path: 'https://juejin.im/user/57a48b632e958a006691b946/posts', title: '掘金', icon: 'globe' },
-  //         { path: 'https://my.oschina.net/u/3871516', title: '开源中国', icon: 'globe' },
-  //         { path: 'https://www.zhihu.com/people/fairy-ever/activities', title: '知乎', icon: 'globe' },
-  //         { path: 'https://segmentfault.com/blog/liyang-note-book', title: 'segmentfault 专栏', icon: 'globe' },
-  //         { path: 'http://www.fairyever.com/', title: 'www.fairyever.com', icon: 'globe' }
-  //       ]
-  //     }
-  //   ]
-  // },
-  //demoPlayground,
+
   // demoBusiness,
   demotrade,
   demobacktest,
-  demorealtime
+  demorealtime,
+  demoPlayground,
+  //demoBusiness,
+  {
+    title: '功能',
+    icon: 'puzzle-piece',
+    children: [
+      demoD2Crud,
+      demoComponents,
+      demoElement,
+      demoCharts,
+      demoPlugins,
+      demoFrame,
+      {
+        title: '新窗口打开链接',
+        icon: 'link',
+        children: [
+          { path: 'https://github.com/d2-projects/d2-admin', title: 'D2Admin Github', icon: 'github' },
+          { path: 'https://juejin.im/user/57a48b632e958a006691b946/posts', title: '掘金', icon: 'globe' },
+          { path: 'https://my.oschina.net/u/3871516', title: '开源中国', icon: 'globe' },
+          { path: 'https://www.zhihu.com/people/fairy-ever/activities', title: '知乎', icon: 'globe' },
+          { path: 'https://segmentfault.com/blog/liyang-note-book', title: 'segmentfault 专栏', icon: 'globe' },
+          { path: 'http://www.fairyever.com/', title: 'www.fairyever.com', icon: 'globe' }
+        ]
+      }
+    ]
+  },
 ]
--- src/menu/modules/demo-realtime.js
+++ src/menu/modules/demo-realtime.js
@@ -3,10 +3,10 @@ export default {
   title: '实时大屏',
   icon: 'flask',
   children: (pre => [
-    { path: `${pre}futuremonitor`, title: '实时期货大屏', icon: 'home' },
-    { path: `${pre}stockmonitor`, title: '实时股票大屏', icon: 'home' },
-    { path: `${pre}trademonitor`, title: '实时交易大屏', icon: 'home' },
-    { path: `${pre}riskmonitor`, title: '实时风控大屏', icon: 'home' }
+    { path: `${pre}futuremonitor`, title: '实时期货大屏', iconSvg: 'cluster' },
+    { path: `${pre}stockmonitor`, title: '实时股票大屏', iconSvg: 'center' },
+    { path: `${pre}trademonitor`, title: '实时交易大屏',iconSvg: 'tradepanel' },
+    { path: `${pre}riskmonitor`, title: '实时风控大屏', iconSvg: 'riskpanel' }
   //   {
   //     title: 'ISSUES',
   //     icon: 'github',
